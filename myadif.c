e249d755b1ca242230f3a348b982f1d727e44ac1
Gianluca Mazzini <gianluca@mazzini.org>
2015-10-04 06:29:08 +0200

    Update myadif.c

--- myadif.c
+++ myadif.c
@@ -6,15 +6,18 @@
 
 int main(){
 	FILE *fpin,*fpout;
-	char *buf[100];
+	char buf[100];
+	
 	// inizialization
 	fpin=fopen(login,"rt");
 	fpout=fopen(logout,"wt");
+	
 	// header
 	fprintf(fpout,"<adif_ver:5>3.0.4\n");
 	fprintf(fpout,"<programid:6>ik4lzh\n");
 	fprintf(fpout,"<eoh>\n");
 	fprintf(fpout,"\n");
+	
 	// log processing
 	for(;;){
 		if(fgets(buf,100,fpin)==NULL)break;
