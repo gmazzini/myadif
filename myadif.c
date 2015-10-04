b4e6740bb0c66986205824d9c9d242971d4ce260
Gianluca Mazzini <gianluca@mazzini.org>
2015-10-04 06:19:28 +0200

    Update myadif.c

--- myadif.c
+++ myadif.c
@@ -1,8 +1,22 @@
 // myadif v0.1 by GM 2015
 
 #include "stdio.h"
+#define login "/Users/gmazzini/Downloads/log.txt"
+#define logout "/Users/gmazzini/Downloads/log.adif"
 
-void main(){
+int main(){
 	FILE *fpin,*fpout;
+	// inizialization
+	fpin=fopen(login,"rt");
+	fpout=fopen(logout,"wt");
+	// header
+	fprintf(fpout,"<adif_ver:5>3.0.4\n");
+	fprintf(fpout,"<programid:6>ik4lzh\n");
+	fprintf(fpout,"<eoh>\n");
+	fprintf(fpout,"\n");
+	
+	
+	fclose(fpin);
+	fclose(fpout);
 	
 }
