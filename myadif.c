dec16c1aebceac89a53c4020dcde117ee5a2b023
Gianluca Mazzini <gianluca@mazzini.org>
2015-10-11 21:01:56 +0200

    Update myadif.c

--- myadif.c
+++ myadif.c
@@ -60,7 +60,7 @@ int main(){
 		fprintf(fpout,"<rst_sent:%lu>%s\n",strlen(myrst_sent),myrst_sent);
 		fprintf(fpout,"<rst_rcvd:%lu>%s\n",strlen(myrst_rcvd),myrst_rcvd);
 		fprintf(fpout,"<mode:3>ssb\n");
-		if(myfreq[0]='m'){
+		if(myfreq[0]=='m'){
 			auxi=myfreq+1;
 			auxe=strchr(auxi,'_');
 			*auxe='\0';
