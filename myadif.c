b9e8066cfcf1d6ef8b403debdbab46540e69848a
Gianluca Mazzini <gianluca@mazzini.org>
2015-10-11 21:24:47 +0200

    Update myadif.c

--- myadif.c
+++ myadif.c
@@ -64,12 +64,12 @@ int main(){
 			auxi=myfreq+1;
 			auxe=strchr(auxi,'_');
 			*auxe='\0';
-			fprintf(fpout,"<band:%s>%s\n",strlen(auxi),auxi);
+			fprintf(fpout,"<band:%lu>%s\n",strlen(auxi),auxi);
 			if(*(auxe+1)=='C'){
 				auxi=auxe+2;
 				auxe=strchr(auxi,'_');
 				*auxe='\0';
-				fprintf(fpout,"<src_string:%s>%s\n",strlen(auxi),auxi);
+				fprintf(fpout,"<src_string:%lu>%s\n",strlen(auxi),auxi);
 			}
 		}
 		else {
